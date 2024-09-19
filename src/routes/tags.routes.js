7f472a95edfbd358db536e7ab3d7eb436b4f8ecb
Emanuel <nuelwnt@edu.unifil.br>
2024-09-19 20:33:19 -0300

    first commit

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+
+const TagsController = require('../controllers/TagsController')
+
+const tagsRoutes = Router()
+
+const tagsController = new TagsController()
+
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+tagsRoutes.get("/", ensureAuthenticated,tagsController.index)
+
+
+
+module.exports = tagsRoutes
